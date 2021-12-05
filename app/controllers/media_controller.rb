827f114f071c41603bd28d29cb895f6c5144a26a
Jonathan-Miksis <jmiksis13@gmail.com>
2021-12-05 17:16:42 -0500

    add backend show action/route

--- app/controllers/media_controller.rb
+++ app/controllers/media_controller.rb
@@ -3,4 +3,10 @@ class MediaController < ApplicationController
     media = Medium.all
     render json: media
   end
+
+  def show 
+    media = Medium.find_by(id: params[:id])
+    render json: media
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -14,4 +14,8 @@ Rails.application.routes.draw do
   #for comments
   post "/comments" => "comments#create"
   delete "/comments/:id" => "comments#destroy"
+
+  #for media
+  get "/media" => "media#index"
+  get "/media/:id" => "media#show"
 end
