47eea4e1f5319dbb828124349513443f4869a080
Jonathan-Miksis <jmiksis13@gmail.com>
2021-12-12 17:21:43 -0500

    add additional api calls to pull more data into database

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,44 +8,50 @@
 
 require 'rest-client'
 
-# movies_call = RestClient.get "https://imdb-api.com/en/API/Top250Movies/#{Rails.application.credentials.imdb_api_key}"
-# media_array = JSON.parse(movies_call)["items"]
-# media_array.each do |medium|
-#   Medium.create(
-#     imdb_id: medium["id"],
-#     title: medium["title"],
-#     year: medium["year"],
-#     director: medium["crew"],
-#     cast: medium["crew"],
-#     # genre: medium[""],
-#     # synopsis: medium[""],
-#     rating: medium["imDbRating"]
-#   )
-# end
 
-# tv_call = RestClient.get "https://imdb-api.com/en/API/Top250TVs/#{Rails.application.credentials.imdb_api_key}"
-# media_array = JSON.parse(tv_call)["items"]
-# media_array.each do |medium|
-#   Medium.create(
-#     imdb_id: medium["id"],
-#     title: medium["title"],
-#     year: medium["year"],
-#     director: medium["crew"],
-#     cast: medium["crew"],
-#     # genre: medium[""],
-#     # synopsis: medium[""],
-#     rating: medium["imDbRating"]
-#   )
-# end
+movies_call = RestClient.get "https://imdb-api.com/en/API/Top250Movies/#{Rails.application.credentials.imdb_api_key}"
+media_array = JSON.parse(movies_call)["items"]
+media_array.each do |medium|
+  Medium.create(
+    imdb_id: medium["id"],
+    title: medium["title"],
+    year: medium["year"],
+    cast: medium["crew"],
+    rating: medium["imDbRating"]
+  )
+end
+
+tv_call = RestClient.get "https://imdb-api.com/en/API/Top250TVs/#{Rails.application.credentials.imdb_api_key}"
+media_array = JSON.parse(tv_call)["items"]
+media_array.each do |medium|
+  Medium.create(
+    imdb_id: medium["id"],
+    title: medium["title"],
+    year: medium["year"],
+    cast: medium["crew"],
+    rating: medium["imDbRating"]
+  )
+end
 
 media = Medium.all
 
 media.each do |medium|
-  binding.pry
+  # binding.pry
   id = medium.imdb_id
-  trailer_call = RestClient.get "https://imdb-api.com/en/API/Trailer/#{Rails.application.credentials.imdb_api_key}/#{id}"
-  medium.update(
-    trailer_url: medium["link"]
-    )
-end
 
+  trailer_call = RestClient.get "https://imdb-api.com/en/API/Trailer/#{Rails.application.credentials.imdb_api_key}/#{id}"
+  trailer_response = JSON.parse(trailer_call)
+
+  full_cast_call = RestClient.get "https://imdb-api.com/en/API/FullCast/#{Rails.application.credentials.imdb_api_key}/#{id}"
+  full_cast_response = JSON.parse(full_cast_call)
+
+  pic_synopsis = RestClient.get "https://imdb-api.com/en/API/Title/#{Rails.application.credentials.imdb_api_key}/#{id}"
+  pic_synopsis_response = JSON.parse(pic_synopsis)
+
+  medium.update(
+    trailer_url: trailer_response["link"],
+    director: full_cast_response["directors"]["items"][0]["name"],
+    synopsis: pic_synopsis_response["plot"],
+    artwork_url: pic_synopsis_response["image"],
+  )
+end
